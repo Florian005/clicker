3e1436192ef3ab6bbf2b8940954714ea95543bce
Florian005 <florian.wurger@caramail.fr>
2025-04-02 21:58:19 +0200

    Update clicker.js

--- clicker.js
+++ clicker.js
@@ -18,8 +18,26 @@ function buyUpgrade() {
     }
 }
 
+function buyUpgradeSecond() {
+    if (score >= upgradeCost_second) {
+        score -= upgradeCost_second;
+        increment_second++;
+        upgradeCost_second = Math.floor(upgradeCost_second * 1.5);
+        updateUI();
+    }
+}
+
 function updateUI() {
     document.getElementById("score").innerText = score;
     document.getElementById("increment").innerText = increment_click;
     document.getElementById("cost").innerText = upgradeCost_click;
+    document.getElementById("secondIncrement").innerText = increment_second;
+    document.getElementById("secondCost").innerText = upgradeCost_second;
 }
+
+function addPointsPerSecond() {
+    score += increment_second;
+    updateUI();
+}
+
+setInterval(addPointsPerSecond, 1000);
